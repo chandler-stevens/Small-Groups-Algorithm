53c1566ddeb008addccce62f502a433ea4628421
Chandler Stevens <chandlerstevens22@gmail.com>
2020-03-09 05:09:09 -0700

    Added Helper Functions

--- README.md
+++ README.md
@@ -40,6 +40,66 @@ The required Python libraries for our program include:
 6. Finally, once the program has completed, the set of sets of each group for each night will be displayed.
 
 ## Reflection
-*Write the reflection about getting the small groups in the minimum number of iterations, etc.*
+*The difficulties you faced during this assignment and how you prevailed. Add screenshots of your 
+program running, and add the set of sets for each file. 
+A link to a YouTube video showing your program running with an explanation
+of the steps that you are taking*
+
+In order to find the set of sets of teams in the minimum amount of iterations, we implemented an algorithm that runs in O(n^3) time.
+We initialize the algorithm in the following way: We read the text file and for each team, we added them to a map _peopleNumbers_ with
+the values being 1 if it is a single person and 2 if there is a married couple. We then made another map _peopleUnivisited_ that
+generates a list of all the people who have not visited each host. We also created a directed graph initialized with the number of
+teams, so each team had their own node. We determined how many groups there should be by doing integer division between _n_ (the number
+of individuals) divided by _m_ (the ideal group size entered by the user). We also initialized a list _superList_ that will contain 
+all of the sub-lists, which will be returned as the results to be outputted. The last items we set up was a list of hosts _hostNames_
+who hosted the previous night. 
+The algorithm follows the following logic:
+while there are still people who have not visited every host's house (there are still values in _peopleUnvisited_) O(n)
+  Make a deep copy of the map of unvisited people called _people_ O(n)
+  Initialize a sublist to store the groups for this night called _night_
+  Initialize a queue _guestQueue_ in case a team doesn't fit in a group and goes above the ideal group size _m_
+  for each group to meet at a host's house (counter-controlled loop from i=0 to i=_n/m_-1)  O(n)
+    Initialize a list called _group_ for that group
+    Select a host based on which team has been visited the least as long as they aren't in the _hostNames_ list O(n)
+    Add host to _group_ and to _hostNames_
+    Iterate through _guestQueue_ and add a teams from it if, when added, they will fit the ideal group size _m_ 
+      if a team was added from _guestQueue_, draw an edge from that guest node to the host node they were added to
+    while _group_ is not filled and less than the ideal group size _m_   
+      find the intersection between _peopleUnvisited_ of that host and the _people_ who have not visited that host O(n)
+      select a guest that will fit in _group_, but if one does not fit then they are added to the _guestQueue_ 
+      if there is no one from _people_ who has also not visited, still add a team to fill the group O(n)
+      add a team to _group_ and remove them from _peopleUnvisited_ of their host and _people_
+      if a team was added, draw an edge from that guest node to the host node they were added to
+    add the now filled _group_ to _night_ 
+   if there is anyone left in _people_, distribute them across all the groups so each _group_ is balanced O(m)
+   add that _night_ to _superList_
+   
+   
+The time complexity of this algorithm that runs the minimum amount of iterations is O(n^3), which is the time complexity of our 
+algorithm. When we generate the _peopleUnvisited_ map values, this takes O(n^2) time. After finding the approximate time complexity 
+of each nested loop, we are left with the time complexity of O(n^3 + n^2m + nm + m) which is then reduced to O(n^3) at worst. 
+
+We ran into several difficulties when creating the algorithm for this project. Our first attempt at an algorithm was too focused on
+representing the data conceptually as a matrix rather than a map. Because of this, we attampted to manipulate the matrix and have
+each team visit each host through a process of picking a host and having the subsequent created groups iterate, shift, and rotate 
+to visit each host. However, this was both far too complex and failed to generate a full clique. After realizing that manipulating a
+conceptual matrix of the data would be both inefficient as well as incomplete, we decided to represent the data with maps and queues.
+By utilizing these data structures and looking for intersectios between maps, we managed to generate a full clique efficiently. We 
+still ran into several difficulties during our revised approach, particularly involving changes to the ideal group size. COME BACK TO
+THIS SECTION :)
+
+The following are screenshots of our program running:
+
+The following is a link to a YouTube video of our program running:
+
+The following are the set of sets created by our program for each file:
+group1.txt:
 
 
+group2.txt:
+
+
+group3.txt:
+
+
+    
--- Small-Group.py
+++ Small-Group.py
@@ -1,184 +1,336 @@
-#from os import system
+# Filename: Small-Group.py
+# Title: Small Group Allocation Algorithm Project
+# Description: Generates list of lists of all Bible study small groups
+#              necessary for all members to visit every other member's
+#              home in the minimum number of nights as possible with a
+#              time complexity of O(n^3).
+# Contributors: Elizabeth Myers and Chandler Stevens
+# Last Updated: 3/9/2020
+# GitHub Classroom Private Repository:
+#     https://github.com/csc3430-winter2020/community-small-groups-palpateam
+#
+# Runtime of each data file with m = 4:
+# group1.txt runtime = 144 seconds = 2 minutes 24 seconds
+# group2.txt runtime = 958 seconds = 15 minutes 58 seconds
+# group3.txt runtime =  seconds =  minutes  seconds
 
-#system("pip install -r .\Requirements.txt")
+# OS library for installing Requirements and checking if file exists
+from os import system, path
+
+# Install Requirements, if necessary
+system("pip install -r ./Requirements.txt")
 
 # Python Standard Library
-from collections import OrderedDict
+
+# System library for printing exception error message
+from sys import exc_info
+# Deepcopy to generate deep copy of maps
 from copy import deepcopy
+# Timeit Timeit to measure empiric time performance
+from timeit import timeit
+
+# Override Timeit Template to return the return value of function call
+timeit.template = """
+def inner(_it, _timer{init}):
+    {setup}
+    _t0 = _timer()
+    for _i in _it:
+        retval = {stmt}
+    _t1 = _timer()
+    return _t1 - _t0, retval
+"""
 
 # Requirements
+
+# Networkx for directed graph data structure
 from networkx import nx
-import matplotlib.pyplot as plt
+# Matplotlib Pyplot Pause for controlling animation speed for human convenience
+from matplotlib import pyplot as plt
+# Matplotlib Pylab Show for drawing animation
 from matplotlib import pylab
 
-#filename = input("Please enter the filename: ")
-#fin = open(filename)
-fin = open("group1.txt")
 
-peopleNumbers = OrderedDict()
-peopleUnvisited = OrderedDict()
-n = 0
+# Purpose: Ask user for file, ideal group size, and then generate small groups
+# Parameters: None
+# Returns: Nothing
+def Main():
+    # Ask user for input file until given file exists
+    filename = input("Please enter the filename: ")
+    while not path.isfile(filename):
+        print("File", filename, "not found.")
+        filename = input("Please enter the filename: ")
 
-#m = int(input("Please enter the ideal group size: "))
+    print("\nOpening", filename, " . . .\n")
 
-m = 6
+    # Parse file and generate n, teamCount and peopleUnvisited
+    G, n, teamCount, peopleNumbers, peopleUnvisited = ParseTeamsFile(filename)
 
-G = nx.DiGraph()
-labels = {}
-nodeIndex = 0
+    # Get ideal group size from user
+    m = RequestIdealGroupSize(n)
 
-for line in fin:
-    if "," in line:
-        commaIndex = line.index(",")
-        nameCouple = line[:commaIndex] + " + " + line[commaIndex + 1:-1]
-        peopleNumbers[nameCouple] = 2
-        peopleUnvisited[nameCouple] = []
-        # Place couples at beginning
-        peopleNumbers.move_to_end(nameCouple, False)
-        G.add_node(nameCouple)
-        labels[nodeIndex] = nameCouple
-        n += 2
-    else:
-        nameSingle = line[:-1]
-        peopleNumbers[nameSingle] = 1
-        peopleUnvisited[nameSingle] = []
-        G.add_node(nameSingle)
-        labels[nodeIndex] = nameSingle
-        n += 1
-    nodeIndex += 1
+    print("\nInitiating Small Group Allocation Algorithm . . .\n")
 
-for persons in peopleNumbers:
-    for person in peopleNumbers:
-        if persons != person:
-            peopleUnvisited[persons].append(person)
+    # Generate list of lists while also measuring the runtime performance
+    runtime, superList = timeit(lambda:
+                                AllocateSmallGroups(G, n, teamCount, m,
+                                                    peopleNumbers,
+                                                    peopleUnvisited),
+                                number=1)
 
-pos = nx.circular_layout(G)
-pylab.show()
-fig = pylab.figure()
+    # Print runtime performance
+    print("Allocated all small groups with n =", n, "and m =", m, "in",
+          runtime, "seconds.")
 
-def animate_edge(host, guest):
+    # Print list of lists
+    print("\n\nPrinting entire list of lists of all small groups . . . \n\n")
+    print(superList)
+
+    input("\n\nPress any key to exit . . .")
+
+
+# Purpose: Parse compatible data file and generate graph and metadata
+# Parameters: filename represents the name of the file provided by user
+# Returns: G represents the directed graph data structure
+#          n represents the number of individuals
+#          teamCount represents the number of teams
+#          peopleNumbers represents size of each team
+#          peopleUnvisited represents teams that have not visited each team
+def ParseTeamsFile(filename):
+    # Open file stream using provided filename
+    fin = open(filename)
+
+    # Initialize directed graph data structure
+    G = nx.DiGraph()
+
+    # Initialize number of people to zero
+    n = 0
+    # Get number of teams (n, but with each couple consolidated into one)
+    teamCount = 0
+
+    # Initialize map for team sizes with value 1 for single or 2 for a couple
+    peopleNumbers = {}
+    # Initialize map for list of teams who have not visited each team
+    peopleUnvisited = {}
+
+    # Iterate through file and parse each team
+    for line in fin:
+        try:
+            if "," in line:
+                commaIndex = line.index(",")
+                name = line[:commaIndex] + " + " + line[commaIndex + 1:-1]
+                peopleNumbers[name] = 2
+                n += 2
+            else:
+                name = line[:-1]
+                peopleNumbers[name] = 1
+                n += 1
+
+            peopleUnvisited[name] = []
+            G.add_node(name)
+            teamCount += 1
+        except:
+            print("File Read Error: ", exc_info()[0])
+            input("Press any key to exit . . .")
+            exit(1)
+
+    fin.close()
+
+    for host in peopleNumbers:
+        for guest in peopleNumbers:
+            if host != guest:
+                peopleUnvisited[host].append(guest)
+
+    return G, n, teamCount, peopleNumbers, peopleUnvisited
+
+
+# Purpose: Ask user for valid ideal group size
+# Parameters: n represents the number of individuals
+# Returns: m represents ideal group size greater than 1
+#              and less than or equal to n
+def RequestIdealGroupSize(n):
+    validM = False
+    # Minimum group size is two
+    m = 2
+
+    while not validM:
+        if m < 2:
+            print("Small groups should have at least 2 people.")
+        elif m > n:
+            print("Group size cannot exceed number of people (" + str(n) + ").")
+
+        try:
+            m = int(input("Please enter the ideal group size: "))
+            if 2 <= m <= n:
+                validM = True
+        except ValueError:
+            print("Please enter an integer for the ideal group size.")
+
+    return m
+
+
+# Purpose: Determine intersection of two lists
+# Parameters: lst1 represents the first list
+#             lst2 represents the second list
+# Returns: A list of elements from lst1 that also exist in lst2
+def Intersection(lst1, lst2):
+    return list(set(lst1) & set(lst2))
+
+
+# Purpose: Determine values unique to a list
+# Parameters: src represents the source list
+#             blacklist represents the blacklist of unwanted elements
+# Returns: A list of elements from src that do not exist in blacklist
+def Difference(src, blk):
+    return list(set(src) - set(blk))
+
+
+# Purpose: Adds an edge to a graph and updates animation
+# Parameters: G represents the directed graph data structure
+#             fig represents the animation canvas figure
+#             pos represents the
+#             guest represents the vertex to draw arrow from
+#             host represents the vertex to draw arrow to
+# Returns: Nothing
+def AnimateEdge(G, fig, pos, guest, host):
     fig.clear()
     G.add_edge(host, guest)
     nx.draw_circular(G, with_labels=True)
     nx.draw_networkx_edges(G, pos, edgelist=[(host, guest)],
-                           edge_color='g', width=5,
-                           node_color=range(24), cmap=plt.cm.Blues)
-    print(G.number_of_edges())
+                           edge_color='g', width=5)
     fig.canvas.draw()
     pylab.draw()
-    #plt.pause(0.1)
+    plt.pause(0.1)
 
-numGroups = n // m
 
-def intersection(lst1, lst2):
-    return list(set(lst1) & set(lst2))
+# Purpose: Allocate teams to small groups in which every team visits every team
+# Parameters: G represents the directed graph data structure
+#             n represents the number of individuals
+#             teamCount represents the number of teams
+#             m represents the ideal group size
+#             peopleNumbers represents size of each team
+#             peopleUnvisited represents teams that have not visited each team
+# Returns: Nothing
+def AllocateSmallGroups(G, n, teamCount, m, peopleNumbers, peopleUnvisited):
+    pos = nx.circular_layout(G)
+    pylab.show()
+    fig = pylab.figure()
 
-superList = []
+    cliqueEdges = teamCount * (teamCount - 1)
 
-nightNum = 0
-previousHosts = []
-while len(sum(list(peopleUnvisited.values()), [])) > 0:
-    people = deepcopy(peopleUnvisited)
-    night = [[]]
-    guestQueue = []
-    hostNames = []
+    numGroups = n // m
 
-    for i in range(numGroups):
-        group = []
-        host = max(people, key=lambda k: len(people[k]))
-        hostQueue = []
-        while host in previousHosts:
-            hostQueue.append(host)
-            del people[host]
-            host = max(people, key=lambda k: len(people[k]))
-        del people[host]
-        group.append(host)
-        night[0].append(peopleNumbers[host])
-        hostNames.append(host)
-        
-        for prevHost in hostQueue:
-            people[prevHost] = []
-        
-        if len(guestQueue) > 0:
-            guestToAdd = guestQueue.pop()
-            group.append(guestToAdd)
-            animate_edge(host, guestToAdd)
-            night[0][i] += peopleNumbers[guestToAdd]
-            if guestToAdd in peopleUnvisited[host]:
-                peopleUnvisited[host].remove(guestToAdd)
+    superList = []
+    nightNum = 0
+    previousHosts = []
+    while len(sum(list(peopleUnvisited.values()), [])) > 0:
+        people = deepcopy(peopleUnvisited)
+        night = [[]]
+        guestQueue = []
+        hostNames = []
 
-        while night[0][i] < m and len(list(people.keys())) > 0:
-            guestToAdd = ""
-            potentialGuests = intersection(peopleUnvisited[host],
-                                           list(people.keys()))
-            noSingles = True
-            for guest in potentialGuests:
-                if peopleNumbers[guest] == 1:
-                    noSingles = False
-                if peopleNumbers[guest] + night[0][i] <= m:
-                    guestToAdd = guest
-                    break
-                else:
-                    guestQueue.append(guest)
-                    del people[guest]
-                    
-            if guestToAdd == "" and noSingles and len(potentialGuests) > 0:
-                guestToAdd = potentialGuests[0]
-                if guestToAdd not in people:
-                    people[guestToAdd] = []
-                if guestToAdd in guestQueue:
-                    guestQueue.remove(guestToAdd)
-                    
-            #print(host, potentialGuests)
-            if (len(potentialGuests) == 0 and
-                len(list(people.keys())) > 0):                
-                guestToAdd = list(people.keys())[0]
-                potentialIndex = 1
-                while (potentialIndex < len(list(people.keys())) and
-                       peopleNumbers[guestToAdd] + night[0][i] > m):                    
-                    guestToAdd = list(people.keys())[potentialIndex]
-                    potentialIndex += 1
-                    
-            #print(host, guestToAdd)
-            if guestToAdd != "":
-                group.append(guestToAdd)
-                animate_edge(host, guestToAdd)
-                night[0][i] += peopleNumbers[guestToAdd]
-                del people[guestToAdd]
-                if guestToAdd in peopleUnvisited[host]:
-                    peopleUnvisited[host].remove(guestToAdd)
+        for i in range(numGroups):
+            group = []
+            potentialHosts = {}
+            for person in Difference(list(people.keys()), previousHosts):
+                potentialHosts[person] = people[person]
 
-        night.append(group)
-
-    for guest in guestQueue:
-        people[guest] = []
-
-    remainingGuests = list(people.keys())
-    if len(remainingGuests) > 0:
-        sortedRemainingGuests = []
-        for guest in remainingGuests:
-            if peopleNumbers[guest] == 2:
-                sortedRemainingGuests.insert(0, guest)  
+            if len(potentialHosts) > 0:
+                host = max(potentialHosts, key=lambda k: len(potentialHosts[k]))
+                del people[host]
             else:
-                sortedRemainingGuests.append(guest)
-        print("RE", sortedRemainingGuests)
-        for guest in sortedRemainingGuests:
-            smallestGroupIndex = night[0].index(min(night[0]))
-            night[smallestGroupIndex + 1].append(guest)
-            night[0][smallestGroupIndex] += peopleNumbers[guest]
+                if len(guestQueue) > 0:
+                    host = guestQueue.pop()
+                else:
+                    host = Intersection(list(people.keys()),
+                                        Difference(previousHosts, hostNames))[0]
+                    del people[host]
 
-    superList.append(night)
+            group.append(host)
+            night[0].append(peopleNumbers[host])
+            hostNames.append(host)
 
-    previousHosts = hostNames
+            for guest in guestQueue:
+                if peopleNumbers[guest] + night[0][i] <= m:
+                    group.append(guest)
+                    AnimateEdge(G, fig, pos, guest, host)
+                    night[0][i] += peopleNumbers[guest]
+                    if guest in peopleUnvisited[host]:
+                        peopleUnvisited[host].remove(guest)
+                    guestQueue.remove(guest)
+                    break
 
-    del people
+            while night[0][i] < m and len(list(people.keys())) > 0:
+                guestToAdd = ""
+                potentialGuests = Intersection(peopleUnvisited[host],
+                                               list(people.keys()))
+                noSingles = True
+                for guest in potentialGuests:
+                    if peopleNumbers[guest] == 1:
+                        noSingles = False
+                    if peopleNumbers[guest] + night[0][i] <= m:
+                        guestToAdd = guest
+                        break
+                    else:
+                        guestQueue.append(guest)
+                        del people[guest]
 
-    nightNum += 1
-    
-    print("Night #", nightNum)
-    print("Group sizes are =", night[0])
-    for j, group in enumerate(night[1:], start=1):
-        print("Group #", j, "=", night[j])
-    print()
+                if guestToAdd == "" and noSingles and len(potentialGuests) > 0:
+                    guestToAdd = potentialGuests[0]
+                    if guestToAdd in guestQueue:
+                        guestQueue.remove(guestToAdd)
+                        people[guestToAdd] = []
+                elif len(potentialGuests) == 0 and len(list(people.keys())) > 0:
+                    guestToAdd = list(people.keys())[0]
+                    potentialIndex = 1
+                    while (potentialIndex < len(list(people.keys())) and
+                           peopleNumbers[guestToAdd] + night[0][i] > m):
+                        guestToAdd = list(people.keys())[potentialIndex]
+                        potentialIndex += 1
 
-input("")
+                if guestToAdd != "":
+                    group.append(guestToAdd)
+                    AnimateEdge(G, fig, pos, guestToAdd, host)
+                    night[0][i] += peopleNumbers[guestToAdd]
+                    del people[guestToAdd]
+                    if guestToAdd in peopleUnvisited[host]:
+                        peopleUnvisited[host].remove(guestToAdd)
+
+            night.append(group)
+
+        for guest in guestQueue:
+            people[guest] = []
+
+        remainingGuests = list(people.keys())
+        if len(remainingGuests) > 0:
+            sortedRemainingGuests = []
+            for guest in remainingGuests:
+                if peopleNumbers[guest] == 2:
+                    sortedRemainingGuests.insert(0, guest)
+                else:
+                    sortedRemainingGuests.append(guest)
+            for guest in sortedRemainingGuests:
+                smallestGroupIndex = night[0].index(min(night[0]))
+                night[smallestGroupIndex + 1].append(guest)
+                night[0][smallestGroupIndex] += peopleNumbers[guest]
+
+        superList.append(night)
+
+        previousHosts = hostNames
+
+        nightNum += 1
+
+        edges = G.number_of_edges()
+
+        print("Night #", nightNum)
+        print("Group sizes are =", night[0])
+        for j, group in enumerate(night[1:], start=1):
+            print("Group #", j, "=", night[j])
+
+        print("Clique is", int(100 * (edges / cliqueEdges)),
+              "% complete (" + str(edges), "of",
+              str(cliqueEdges) + " edges).\n")
+
+    return superList
+
+
+Main()
